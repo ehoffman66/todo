a5931ddbce847c595b90f3974b58dd3bf708ba51
ehoffman66 <erik.m.hoffman@gmail.com>
2024-01-16 19:57:42 -0500

    Update the styling on the stats box

--- src/App.js
+++ src/App.js
@@ -338,13 +338,14 @@ function App() {
                      <Card 
                         heading={<span style={{ fontSize: '2em' }}>STATS</span>} 
                         paragraph={
-                        <div>
-                           <p>Total Todos: {todos.length}</p>
-                           <p>Completed Todos: {todos.filter(todo => todo.completed).length}</p>
-                           <p>Uncompleted Todos: {todos.filter(todo => !todo.completed).length}</p>
-                           <p>Unique Categories: {new Set(todos.map(todo => todo.category)).size}</p>
-                        </div>
+                           <div>
+                              <p>Total Todos: {todos.length}</p>
+                              <p>Completed Todos: {todos.filter(todo => todo.completed).length}</p>
+                              <p>Uncompleted Todos: {todos.filter(todo => !todo.completed).length}</p>
+                              <p>Unique Categories: {new Set(todos.map(todo => todo.category)).size}</p>
+                           </div>
                         }
+                        size="xl:w-full" 
                      />
                   </div>
                </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,12 @@ import React from 'react';
 type Props = {
   heading: string,
   paragraph: React.ReactNode,
+  size: string,
 }
 
-const Card = ({ heading, paragraph }: Props) => {
+const Card = ({ heading, paragraph, size = 'w-full' }: Props) => {
     return (
-      <div className="w-full rounded-md border-2 border-black bg-[#bc95d4] font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">
+      <div className={`${size} rounded-md border-2 border-black bg-[#bc95d4] font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] mx-auto`}>
         <div className="border-b-2 border-black p-4">
           <h2 className="text-lg">{heading}</h2>
         </div>
@@ -16,6 +17,6 @@ const Card = ({ heading, paragraph }: Props) => {
         </div>
       </div>
     );
-  };
+};
 
 export default Card;
