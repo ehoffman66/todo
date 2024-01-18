1211cabdf4fb1327fc047392f4b6bf0bf7aa0b0d
ehoffman66 <erik.m.hoffman@gmail.com>
2024-01-18 17:06:19 -0500

    Update tailwind.config.js. Fix the warning for Tailwind CSS v3.0 and simplified config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-};
-
-module.exports = {
-  // ...
-  theme: {
-    extend: {
-      colors: {
-        'brand-black': '#000000',
-        'brand-gray': '#E5E5E5',
-        'brand-orange': '#F2994A',
-      },
-      fontFamily: {
-        'sans': ['Inter', 'sans-serif'], // Replace 'Inter' with the font used in the design if different
-      },
-    },
-  },
-  // ...
-};
-
-module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
@@ -40,7 +10,7 @@ module.exports = {
         'brand-orange': '#F2994A',
       },
       fontFamily: {
-        'sans': ['Inter', 'sans-serif'], // Replace 'Inter' with the font used in the design if different
+        'sans': ['Inter', 'sans-serif'],
       },
     },
   },
@@ -48,4 +18,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
