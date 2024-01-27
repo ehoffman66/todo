ae7bdba655227ecc25a0916fcb3d3156beacf113
ehoffman66 <erik.m.hoffman@gmail.com>
2024-01-26 21:00:53 -0500

    Update it so links are detected and clickable and that a todo item is only marked as complete when a user clicks the checkbox

--- src/App.js
+++ src/App.js
@@ -101,6 +101,24 @@ function App() {
       }
    };
 
+   function linkify(inputText) {
+      let replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+      //URLs starting with http://, https://, or ftp://
+      replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gim;
+      replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+      //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+      replacePattern2 = /(^|[^/])(www.[\S]+(\b|$))/gim;
+      replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+      //Change email addresses to mailto:: links.
+      replacePattern3 = /(([a-zA-Z0-9-.])+@[a-zA-Z0-9-.]+\.[a-zA-Z0-9]{2,5})/gim;
+      replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+      return replacedText;
+   }
+
    const handleUpdateTodo = (id, newText, newCategory, newDueDate) => {
       setTodos(todos.map(todo => 
          todo.id === id 
@@ -367,7 +385,7 @@ function App() {
                                     </form>
                                  ) : (
                                     <Checkbox 
-                                       item={todo.text} 
+                                       item={<div dangerouslySetInnerHTML={{__html: linkify(todo.text)}} />} 
                                        checked={todo.completed} 
                                        onChange={() => toggleCompletion(todo.id)}
                                        style={{ fontSize: '5.2em' }}
