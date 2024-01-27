94b5c750bd6f33b7cbe86b16a169e3803db59bd4
ehoffman66 <erik.m.hoffman@gmail.com>
2024-01-26 21:03:52 -0500

    Fix style of checkboxes

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -4,12 +4,14 @@ import { MdClose } from 'react-icons/md';
 const Checkbox = ({ item, checked, onChange }) => {
     return (
         <div className="my-2 flex items-center font-bold">
-            <input
-                type="checkbox"
-                checked={checked}
-                onChange={onChange}
-                className="mr-2"
-            />
+            <button
+                onClick={onChange}
+                className="mr-2.5 grid h-5 w-5 place-items-center rounded-[5px] bg-white outline outline-5 outline-black"
+                role="checkbox"
+                aria-checked={checked}
+            >
+                {checked && <MdClose className="h-4 w-4" style={{ transform: 'scale(1.5)' }} />}
+            </button>
             <p className="text-left" style={{ fontSize: '1.2em', textDecoration: checked ? 'line-through' : 'none' }}>
                 {item}
             </p>
