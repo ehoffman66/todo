f01a7bc26b6f729d5af512e5e2403284a4eb4ec3
ehoffman66 <erik.m.hoffman@gmail.com>
2024-02-09 18:48:15 -0500

    Update Badge component styling

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -7,7 +7,7 @@ export default function Badge({ badgeText, onClick, isSelected, badgeColor }) {
 
     return (
         <div 
-            className={`my-1 w-min rounded-full border-2 border-black px-3 py-1.5 text-sm font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all ${isSelected ? 'translate-x-[3px] translate-y-[3px] shadow-none' : 'hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none'} cursor-pointer`}
+            className={`text-left my-1 w-min rounded-full border-2 border-black px-3 py-1.5 text-sm font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all ${isSelected ? 'translate-x-[3px] translate-y-[3px] shadow-none' : 'hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none'} cursor-pointer`}
             style={{ backgroundColor: badgeColor }}
             onClick={handleClick}
         >
