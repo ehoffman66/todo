24fdd5a69e26a7d2e943c81ce033c5df8adc4aa8
ehoffman66 <erik.m.hoffman@gmail.com>
2024-02-09 19:07:10 -0500

    Refactor URL display in App.js

--- src/App.js
+++ src/App.js
@@ -181,14 +181,16 @@ function App() {
       // URLs starting with http://, https://, or ftp://
       replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gim;
       replacedText = inputText.replace(replacePattern1, (match, url) => {
-         const displayText = url.replace(/^https?:\/\//, ''); // Remove "http://" or "https://" from the start of the URL
+         let displayText = url.replace(/^https?:\/\//, ''); // Remove "http://" or "https://" from the start of the URL
+         displayText = displayText.length > 40 ? displayText.substr(0, 40) + '...' : displayText;
          return '<a href="' + url + '" target="_blank" style="text-decoration: underline;">' + displayText + '</a>';
       });
 
       // URLs starting with "www." (without // before it).
       replacePattern2 = /(^|[^/])(www.[\S]+(\b|$))/gim;
       replacedText = replacedText.replace(replacePattern2, (match, p1, p2) => {
-         const displayText = p2.replace(/^www\./, ''); // Remove "www." from the start of the URL
+         let displayText = p2.replace(/^www\./, ''); // Remove "www." from the start of the URL
+         displayText = displayText.length > 40 ? displayText.substr(0, 40) + '...' : displayText;
          return p1 + '<a href="http://' + p2 + '" target="_blank" style="text-decoration: underline;">' + displayText + '</a>';
       });
 
