46f730f2708be42a6295b0a13606dbaaf597b60a
ehoffman66 <erik.m.hoffman@gmail.com>
2024-02-13 18:37:05 -0500

    Remove unused MongoDB connection options

--- server.js
+++ server.js
@@ -26,8 +26,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
   }).then(() => {
     console.log('Connected to MongoDB');
   }).catch(err => {
