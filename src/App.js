145a109cd29b8abef2f58cfb7a1eb6a0bd7cd732
ehoffman66 <erik.m.hoffman@gmail.com>
2024-02-16 14:35:38 -0500

    Add linkify utility function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { FaListAlt } from 'react-icons/fa';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Checkbox, Button, Input, Card, Badge, Select, SettingsCard, StatsCard, GoalsCard, UserCard } from './components';
 import ReactGA from 'react-ga';
+import { linkify } from './utils';
 
 ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
 
@@ -149,30 +150,6 @@ function App() {
          setEditDueDate('');
       }
    };
-
-   function linkify(inputText) {
-      let replacedText, replacePattern1, replacePattern2;
-
-      // URLs starting with http://, https://, or ftp://
-      replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gim;
-      replacedText = inputText.replace(replacePattern1, (match, url) => {
-         let displayText = url.replace(/^https?:\/\//, ''); // Remove "http://" or "https://" from the start of the URL
-         displayText = displayText.replace(/\/$/, ''); // Remove trailing slash
-         displayText = displayText.length > 40 ? displayText.substr(0, 40) + '...' : displayText;
-         return '<a href="' + url + '" target="_blank" style="text-decoration: underline;">' + displayText + '</a>';
-      });
-
-      // URLs starting with "www." (without // before it).
-      replacePattern2 = /(^|[^/])(www.[\S]+(\b|$))/gim;
-      replacedText = replacedText.replace(replacePattern2, (match, p1, p2) => {
-         let displayText = p2.replace(/^www\./, ''); // Remove "www." from the start of the URL
-         displayText = displayText.replace(/\/$/, ''); // Remove trailing slash
-         displayText = displayText.length > 40 ? displayText.substr(0, 40) + '...' : displayText;
-         return p1 + '<a href="http://' + p2 + '" target="_blank" style="text-decoration: underline;">' + displayText + '</a>';
-      });
-
-      return replacedText;
-   }
 
    const handleUpdateTodo = async (id, newText, newCategory, newDueDate) => {
       if (!id) {
