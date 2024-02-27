1b111321540b9a15b2d830f2e9e504f6c610ed54
thirlestane-danielh <danablend@gmail.com>
2024-02-27 01:01:15 +0000

    Improve file indentation

--- src/components/Input.js
+++ src/components/Input.js
@@ -13,13 +13,15 @@ interface InputProps {
 const Input: React.FC<InputProps> = ({ id, label, value, setValue, placeholder, type = 'text', size = '500px' }) => {
     return (
         <div>
-            <label htmlFor={id} className="visually-hidden">{label}</label>
+            <label htmlFor={id} className='visually-hidden'>
+                {label}
+            </label>
             <input
                 id={id}
-                className="text-left mr-[20px] rounded-md border-2 border-black p-[10px] font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] outline-none transition-all focus:translate-x-[3px] focus:translate-y-[3px] focus:shadow-none"
+                className='text-left mr-[20px] rounded-md border-2 border-black p-[10px] font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] outline-none transition-all focus:translate-x-[3px] focus:translate-y-[3px] focus:shadow-none'
                 style={{ width: size }}
                 type={type}
-                name="text"
+                name='text'
                 placeholder={placeholder}
                 value={value}
                 onChange={(e) => {
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 function LoginPage() {
     const handleLogin = () => {
         window.location.href = 'http://localhost:3000/auth/google';
-    }
+    };
 
     return (
         <div>
