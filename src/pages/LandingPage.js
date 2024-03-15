b74ae3edc07c4e9c3973c612b6aaf5463fbd6305
ehoffman66 <erik.m.hoffman@gmail.com>
2024-03-15 19:33:38 -0400

    Add LandingPage.css and LandingPage.js files

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from '../components/common/Button';
+import { FaGoogle } from 'react-icons/fa';
+
+const LandingPage = () => {
+
+    return (
+        <div className='flex flex-col items-center justify-center h-screen bg-purple-300'>
+            <h1 className='mb-0 leading-none' style={{ fontFamily: 'Kanit', fontSize: '15rem' }}>Brutal</h1>
+            <h1 className='mt-0 mb-4 leading-none' style={{ fontFamily: 'Kanit', fontSize: '20rem' }}>Tasks</h1>
+            <p className='mb-8 text-xl'>Conquer Your Day, The Brutal Way</p>
+            <Button onClick={() => (window.location.href = `${process.env.REACT_APP_SERVER_URL}/auth/google`)} color="bc95d4">
+                <FaGoogle className='mr-2' /> Login
+            </Button>
+        </div>
+    );
+};
+
+export default LandingPage;
