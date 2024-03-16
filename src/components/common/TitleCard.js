e8b096657c1a04474639fbcc3c53669770953406
ehoffman66 <erik.m.hoffman@gmail.com>
2024-03-15 23:04:43 -0400

    Refactor TitleCard component to improve text alignment and font sizes

--- src/components/common/TitleCard.js
+++ src/components/common/TitleCard.js
@@ -7,9 +7,10 @@ const TitleCard = ({ cardColor }) => {
             <Card
                 backgroundColor={cardColor}
                 heading={
-                    <div className='text-left'>
-                        <div className='text-5xl font-kanit'>Brutal</div>
-                        <div className='text-7xl font-kanit'>Tasks</div>
+                    <div className='sm:text-left text-center'>
+                        <span className='text-6xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-kanit'>Brutal</span>
+                        <span className='hidden sm:inline'> </span>
+                        <span className='text-7xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-7xl font-kanit'>Tasks</span>
                     </div>
                 }
                 showDivider={false}
