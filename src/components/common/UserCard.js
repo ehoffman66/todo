0a843ffeb5a422741d5ad8fd45c3e7b083bc28b3
ehoffman66 <erik.m.hoffman@gmail.com>
2024-03-16 12:35:48 -0400

    Add Card component and update imports. Change extension to tsx

--- src/components/common/UserCard.js
+++ src/components/common/UserCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from './Card';
+import Card from './Card.tsx';
 import Avatar from './Avatar';
 import Button from './Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
